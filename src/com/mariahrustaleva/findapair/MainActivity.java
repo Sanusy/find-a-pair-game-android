c16f0cbce21f14ea6482a75638bde35d3d1f00f2
Boris Etingof <boba@Boriss-MacBook-Pro.local>
2014-12-20 19:53:59 +1100

    Finished and combined first two

--- src/com/mariahrustaleva/findapair/MainActivity.java
+++ src/com/mariahrustaleva/findapair/MainActivity.java
@@ -32,7 +32,13 @@ public class MainActivity extends Activity {
 	private Drawable backImage;
 	private Card[][] cards;
 	private List<Drawable> images;
-	private GroupButtonListener cardCardButtonListener;
+	
+	private OnClickListener cardButtonListener;
+	private OnClickListener groupButtonListener;
+	private OnClickListener cardListener;
+	
+	private OnClickListener selectGameListener;
+	
 	private TableLayout mainTable;
 	private TableRow game_grid_container;
 	private TextView countdown_view;
@@ -41,7 +47,15 @@ public class MainActivity extends Activity {
 	private TextView end_game_message;
 	private TableRow reset_btn_container;
 	private Button reset_btn;
-
+	
+	private TableRow normal_game_btn_container;
+	private TableRow group_game_btn_container;
+	private TableRow pvp_game_btn_container;
+	
+	private Button normal_game_btn;
+	private Button group_game_btn;
+	private Button pvp_game_btn;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -57,9 +71,25 @@ public class MainActivity extends Activity {
 		countdown_view = (TextView) findViewById(R.id.countdown);
 		score_view = (TextView) findViewById(R.id.score);
 		context = mainTable.getContext();
-
+		
+		normal_game_btn_container = (TableRow) findViewById(R.id.normal_game_btn_container);
+		group_game_btn_container = (TableRow) findViewById(R.id.group_game_btn_container);
+		pvp_game_btn_container = (TableRow) findViewById(R.id.pvp_game_btn_container);
+		
+		normal_game_btn = (Button) findViewById(R.id.normal_game_btn);
+		group_game_btn = (Button) findViewById(R.id.group_game_btn);
+		pvp_game_btn = (Button) findViewById(R.id.pvp_game_btn); 
+		
 		// add listeners
-		cardCardButtonListener = new GroupButtonListener();
+		cardButtonListener = new CardButtonListener();
+		groupButtonListener = new GroupButtonListener();
+		
+		selectGameListener = new SelectGameListener();
+		
+		normal_game_btn.setOnClickListener(selectGameListener);
+		group_game_btn.setOnClickListener(selectGameListener);
+		pvp_game_btn.setOnClickListener(selectGameListener);
+		
 		reset_btn.setOnClickListener(new ResetButtonListener());
 
 		loadImages();
@@ -78,13 +108,42 @@ public class MainActivity extends Activity {
 			images.add(drawable);
 		}
 	}
-
+	
+	
 	private void newGame() {
+		score_view.setVisibility(View.INVISIBLE);
+		reset_btn_container.setVisibility(View.GONE);
+		game_grid_container.setVisibility(View.GONE);
+		end_game_message_container.setVisibility(View.GONE);
+		
+		normal_game_btn_container.setVisibility(View.VISIBLE);
+		group_game_btn_container.setVisibility(View.VISIBLE);
+		pvp_game_btn_container.setVisibility(View.VISIBLE);
+	}
+
+	private void startGame(String gameType) {
+		if(gameType == "normal") {
+			cardListener = cardButtonListener;
+		}
+		else if(gameType == "group") {
+			cardListener = groupButtonListener;
+		}
+		else {
+			// TODO add pvp logic
+		}
+
+		score_view.setVisibility(View.VISIBLE);
+		
+		normal_game_btn_container.setVisibility(View.GONE);
+		group_game_btn_container.setVisibility(View.GONE);
+		pvp_game_btn_container.setVisibility(View.GONE);
+		
 		// reset UI
+		score_view.setVisibility(View.VISIBLE);
 		score_view.setText(0 + ":" + 0);
 		game_grid_container.setVisibility(View.VISIBLE);
 		end_game_message_container.setVisibility(View.GONE);
-		reset_btn_container.setVisibility(View.GONE);
+
 
 		// create deck
 		createShuffledDeck();
@@ -151,7 +210,7 @@ public class MainActivity extends Activity {
 	private Card createCard(int id, int group_id) {
 		Card card = new Card(context, id, group_id);
 		card.setBackgroundDrawable(backImage);
-		card.setOnClickListener(cardCardButtonListener);
+		card.setOnClickListener(cardListener);
 		return card;
 	}
 
@@ -180,8 +239,158 @@ public class MainActivity extends Activity {
 			newGame();
 		}
 	}
+	
+	// listener for reset button
+	class SelectGameListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			if(v == normal_game_btn){
+				startGame("normal");
+			}
+			
+			else if(v == group_game_btn){
+				startGame("group");
+			}
+			
+			else {
+				startGame("pvp");
+			}
+		}
+	}
 
+	//SINGLE PAIRS LOGIC START
 	// listener for a card button
+	class CardButtonListener implements OnClickListener {
+		Card matching_card;
+		Card current_card;
+		
+		private int computer_score;
+		private int user_score;
+		private int time_count;
+		
+		private ScheduledFuture future_task;
+		private ScheduledExecutorService countdown_timer = Executors
+				.newSingleThreadScheduledExecutor();
+
+		@Override
+		public void onClick(View v) {
+			synchronized (lock) {
+				
+				Card card = (Card)v; 
+				
+				// turning a card (no match available)  
+				if(matching_card == null){
+					// Iterate through the cards array to find matching card
+					Card c;
+					for (int y = 0; y < ROW_COUNT; y++) {
+						for (int x = 0; x < COL_COUNT; x++) {
+							c = cards[y][x];
+							
+							// if there's a match available, store it and set the countdown timer
+							if(c.isUp && c.id == card.id && c != card){
+								
+								//store the match
+								matching_card = c;
+								current_card = card; 
+								
+								// start timer
+								time_count = MAX_TIME;
+								future_task = countdown_timer.scheduleAtFixedRate(new Task(), 0, 1000, TimeUnit.MILLISECONDS);
+							}
+						}
+					}
+					
+					// turn the card over
+					card.setBackgroundDrawable(images.get(card.id));
+					card.isUp = true;
+				}
+				
+				// match is available, user clicks a matching card
+				else if(card == matching_card){
+					user_score ++;
+					endTurn();
+				}
+			}
+		}
+		
+		// implementation for countdown timer
+		// tics every second
+		// when time_count reaches 0 ( initial time_count value = MAX_TIME ) computer gets the score
+		class Task implements Runnable {
+
+			@Override
+			public void run() {
+				
+				// all UI changes need to be run on UI thread, 
+				// so using a wrapper
+				runOnUiThread(new Runnable() {
+				     @Override
+				     public void run() {
+				    	time_count --;
+				    	countdown_view.setText("Time left to find a match: " + time_count);
+						
+						if(time_count == 0) {
+							computer_score ++;
+					    	endTurn();
+						}
+				    	 
+				     }
+				});
+			}
+		}
+		
+		private void endTurn(){
+			future_task.cancel(true);
+			
+			//clear countdown
+			countdown_view.setText("");
+			
+			//update score
+			score_view.setText(user_score  + ":" + computer_score);
+			
+			//remove matching cards
+			matching_card.setVisibility(View.INVISIBLE);
+			current_card.setVisibility(View.INVISIBLE);
+			matching_card = null;
+			current_card = null;
+			
+			//check end game
+			int turns = user_score + computer_score;
+			if(turns == ROW_COUNT * COL_COUNT / 2){
+				String msg;
+				
+				if(user_score < computer_score) {
+					msg = "Defeat...";
+				}
+				else if(user_score > computer_score) {
+					msg = "Victory!";
+				}
+				else {
+					msg = "Draw.";
+				}
+				
+				// show end game messages
+				end_game_message.setText(msg);
+				game_grid_container.setVisibility(View.GONE);
+				end_game_message_container.setVisibility(View.VISIBLE);
+				reset_btn_container.setVisibility(View.VISIBLE);
+				
+				reset();
+			}
+		}
+		
+		private void  reset(){
+			computer_score = 0;
+			user_score = 0;
+			
+			// cancel countdown timer task if it's scheduled
+			if (future_task != null)
+				future_task.cancel(false);
+		}
+	}
+	//SINGLE PAIRS LOGIC END
+	
+	// GROUP PAIRS LOGIC START
 	class GroupButtonListener implements OnClickListener {
 		private int computer_score;
 		private int user_score;
@@ -399,4 +608,9 @@ public class MainActivity extends Activity {
 				future_task.cancel(false);
 		}
 	}
+	//GROUP PAIRS LOGIC END
+	
+	
+
+
 }
