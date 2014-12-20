d1628659ac6b7284a7a1f4bbdb8927c000e80184
Boris Etingof <boba@Boriss-MacBook-Pro.local>
2014-12-20 17:58:27 +1100

    Before combining

--- src/com/mariahrustaleva/findapair/Card.java
+++ src/com/mariahrustaleva/findapair/Card.java
@@ -7,12 +7,15 @@ import android.widget.Button;
 public class Card extends Button{
 
 	public int id;
+	public int group_id;
 	public Boolean isUp = false;
+	public Boolean isOnTable = true;
 
-	public Card(Context context, int id) {
+	public Card(Context context, int id, int group_id) {
 		super(context);
 
 		this.id = id;
+		this.group_id = group_id;
 	}
 
 }
--- src/com/mariahrustaleva/findapair/MainActivity.java
+++ src/com/mariahrustaleva/findapair/MainActivity.java
@@ -26,13 +26,13 @@ public class MainActivity extends Activity {
 	private static int COL_COUNT = 6;
 	private static int MAX_TIME = 5;
 	private static Object lock = new Object();
-	
-	//UI
+
+	// UI
 	private Context context;
 	private Drawable backImage;
 	private Card[][] cards;
 	private List<Drawable> images;
-	private CardButtonListener cardCardButtonListener;
+	private GroupButtonListener cardCardButtonListener;
 	private TableLayout mainTable;
 	private TableRow game_grid_container;
 	private TextView countdown_view;
@@ -41,10 +41,11 @@ public class MainActivity extends Activity {
 	private TextView end_game_message;
 	private TableRow reset_btn_container;
 	private Button reset_btn;
-	
+
 	private int computer_score;
 	private int user_score;
 	private int time_count;
+	
 	private Card current_card;
 	private Card matching_card;
 	private ScheduledFuture future_task;
@@ -59,20 +60,20 @@ public class MainActivity extends Activity {
 		mainTable = (TableLayout) findViewById(R.id.main_table);
 		game_grid_container = (TableRow) findViewById(R.id.game_grid_container);
 		end_game_message_container = (TableRow) findViewById(R.id.end_game_message_container);
-		end_game_message = (TextView)findViewById(R.id.end_game_message);
+		end_game_message = (TextView) findViewById(R.id.end_game_message);
 		reset_btn_container = (TableRow) findViewById(R.id.reset_btn_container);
-		reset_btn =  (Button) findViewById(R.id.reset_btn);
-		countdown_view = (TextView)findViewById(R.id.countdown);
-		score_view = (TextView)findViewById(R.id.score);
+		reset_btn = (Button) findViewById(R.id.reset_btn);
+		countdown_view = (TextView) findViewById(R.id.countdown);
+		score_view = (TextView) findViewById(R.id.score);
 		context = mainTable.getContext();
 
 		// add listeners
-		cardCardButtonListener = new CardButtonListener();
+		cardCardButtonListener = new GroupButtonListener();
 		reset_btn.setOnClickListener(new ResetButtonListener());
-		
+
 		// create countdaown timer
 		countdown_timer = Executors.newSingleThreadScheduledExecutor();
-		
+
 		loadImages();
 		newGame();
 	}
@@ -98,89 +99,85 @@ public class MainActivity extends Activity {
 		user_score = 0;
 
 		// reset UI
-		score_view.setText(user_score  + ":" + computer_score);
+		score_view.setText(user_score + ":" + computer_score);
 		game_grid_container.setVisibility(View.VISIBLE);
 		end_game_message_container.setVisibility(View.GONE);
 		reset_btn_container.setVisibility(View.GONE);
-		
+
 		// cancel countdown timer task if it's scheduled
-		if(future_task != null) future_task.cancel(false);
+		if (future_task != null)
+			future_task.cancel(false);
 
 		// create deck
 		createShuffledDeck();
-		
+
 		// add cards to container view
-		//placeCardsOnTheTable();
+		placeCardsOnTheTable();
 	}
 
 	private void createShuffledDeck() {
-		cards 						= new Card[COL_COUNT][ROW_COUNT];
-		Random random 				= new Random();
-		ArrayList<Integer> listA 	= new ArrayList<Integer>();
-		ArrayList<Integer> listB 	= new ArrayList<Integer>();
-		
-		ArrayList<Integer> two_extra 	= new ArrayList<Integer>();
+		cards = new Card[COL_COUNT][ROW_COUNT];
+		Random random = new Random();
+		ArrayList<Integer> listA = new ArrayList<Integer>();
+		ArrayList<Integer> listB = new ArrayList<Integer>();
 
-		int size = ROW_COUNT * COL_COUNT,
-				rand, val, index, row, col;
-		
+		ArrayList<Integer> two_extra = new ArrayList<Integer>();
 
-		for (int i = 0; i < size/2; i++) {
+		int size = ROW_COUNT * COL_COUNT, rand, val, index, row, col;
+
+		for (int i = 0; i < size / 2; i++) {
 			listA.add(Integer.valueOf(i));
 			listB.add(Integer.valueOf(i));
 		}
-		
+
 		ArrayList<Integer> currentList = listA;
 		ArrayList<Integer> pendingList = listB;
 
-		for (int i = 0; i < size; i+=4) {
+		for (int i = 0; i < size; i += 4) {
 
 			// top left element of every group
-			col = i % COL_COUNT;
-			row = i / COL_COUNT;
-			
-			for(int j = 0; j < 4; j++){
-				rand = currentList.size() > 1 ? random.nextInt(currentList.size()) : 0;
+			col = (i / 4) % (COL_COUNT / 2) * 2;
+			row = (i / 4) / (COL_COUNT / 2) * 2;
+
+			for (int j = 0; j < 4; j++) {
+				rand = currentList.size() > 1 ? random.nextInt(currentList
+						.size()) : 0;
 				val = currentList.remove(rand);
-				
-				//current element of a group
-				int col_i = col/2 + j%2;		
-				int row_i = row/2 + j/2;
-				
-				Log.i(i + " : " + j + " : " + val, "-");
-				
-				if (i == 0 && j < 2 ){
+
+				// store first 2 values
+				if (i == 0 && j < 2) {
 					two_extra.add(pendingList.remove(pendingList.indexOf(val)));
 				}
-				
-				//cards[row_i][col_i] = createCard(val);
+
+				// current element of a group
+				int col_i = col + j % 2;
+				int row_i = row + j / 2;
+
+				cards[col_i][row_i] = createCard(val, i / 4);
 			}
-			
-			if(i == 0 ) {
+
+			if (i == 0) {
 				currentList.add(two_extra.get(0));
 				currentList.add(two_extra.get(1));
 			}
-			
-			if(currentList == listA) {
+
+			if (currentList == listA) {
 				currentList = listB;
-			}
-			else {
+			} else {
 				currentList = listA;
 			}
-			
-			
 
 		}
 	}
-	
-	private Card createCard(int id){
-		Card card = new Card(context, id);
+
+	private Card createCard(int id, int group_id) {
+		Card card = new Card(context, id, group_id);
 		card.setBackgroundDrawable(backImage);
 		card.setOnClickListener(cardCardButtonListener);
 		return card;
 	}
-	
-	private void placeCardsOnTheTable(){
+
+	private void placeCardsOnTheTable() {
 		game_grid_container.removeAllViews();
 
 		mainTable = new TableLayout(context);
@@ -193,11 +190,11 @@ public class MainActivity extends Activity {
 			for (int x = 0; x < COL_COUNT; x++) {
 				row.addView(cards[x][y]);
 			}
-			
+
 			mainTable.addView(row);
 		}
 	}
-	
+
 	// listener for reset button
 	class ResetButtonListener implements OnClickListener {
 		@Override
@@ -205,115 +202,204 @@ public class MainActivity extends Activity {
 			newGame();
 		}
 	}
-	
+
 	// listener for a card button
-	class CardButtonListener implements OnClickListener {
+	class GroupButtonListener implements OnClickListener {
+		private ArrayList<Card> current_group;
+		private ArrayList<Integer> matching_groups_ids = new ArrayList<Integer>();
+
+		private ArrayList<Card> getGroupById(int id) {
+			Card c;
+			ArrayList<Card> card_group = new ArrayList<Card>();
+
+			for (int y = 0; y < ROW_COUNT; y++) {
+				for (int x = 0; x < COL_COUNT; x++) {
+					c = cards[y][x];
+					if (c.group_id == id) {
+						card_group.add(c);
+					}
+				}
+			}
+			return card_group;
+		}
+
+		private ArrayList<Integer> findMatchingGroupsIds(ArrayList<Card> group) {
+			Card c;
+			ArrayList<Integer> result = new ArrayList<Integer>();
+
+			for (Card card : group) {
+				for (int y = 0; y < ROW_COUNT; y++) {
+					for (int x = 0; x < COL_COUNT; x++) {
+						c = cards[y][x];
+
+						if (c.isOnTable && c.isUp && c.id == card.id && c != card
+								&& !result.contains(c.group_id)) {
+							result.add(c.group_id);
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private void turnGroupOver(ArrayList<Card> group) {
+			for (Card card : group) {
+				card.setBackgroundDrawable(images.get(card.id));// bugged here once, coz index was -1
+				card.isUp = true;
+			}
+		}
+
+		private void removeGroupFromTable(ArrayList<Card> group) {
+			for (Card card : group) {
+				card.setVisibility(View.INVISIBLE);
+				card.isOnTable = false;
+			}
+		}
 
 		@Override
 		public void onClick(View v) {
 			synchronized (lock) {
-				
-				Card card = (Card)v; 
-				
-				// turning a card (no match available)  
-				if(matching_card == null){
-					// Iterate through the cards array to find matching card
-					Card c;
-					for (int y = 0; y < ROW_COUNT; y++) {
-						for (int x = 0; x < COL_COUNT; x++) {
-							c = cards[y][x];
-							
-							// if there's a match available, store it and set the countdown timer
-							if(c.isUp && c.id == card.id && c != card){
-								
-								//store the match
-								matching_card = c;
-								current_card = card; 
-								
-								// start timer
-								time_count = MAX_TIME;
-								future_task = countdown_timer.scheduleAtFixedRate(new Task(), 0, 1000, TimeUnit.MILLISECONDS);
-							}
-						}
+
+				Card card = (Card) v;
+
+				if (matching_groups_ids.size() == 0) {
+
+					ArrayList<Card> cg = getGroupById(card.group_id);
+					matching_groups_ids = findMatchingGroupsIds(cg);
+
+					// turn the group over
+					turnGroupOver(cg);
+
+					if (matching_groups_ids.size() != 0) {
+						current_group = cg;
+
+						// start timer
+						time_count = MAX_TIME;
+						future_task = countdown_timer.scheduleAtFixedRate(
+								new Task(), 0, 1000, TimeUnit.MILLISECONDS);
 					}
-					
-					// turn the card over
-					card.setBackgroundDrawable(images.get(card.id));
-					card.isUp = true;
+
+					else {
+						checkEndGame(2000);
+					}
+
 				}
-				
-				// match is available, user clicks a matching card
-				else if(card == matching_card){
-					user_score ++;
-					endTurn();
+
+				// match is available
+				else if (matching_groups_ids.contains(card.group_id)) {
+					user_score++;
+					Log.i("else", "else");
+
+					if (matching_groups_ids.size() == 1) {
+						Log.i("one", "one " + matching_groups_ids.size());
+						endTurn();
+					} else {
+						Log.i("two", "two " + matching_groups_ids.size());
+
+						matching_groups_ids.remove(matching_groups_ids
+								.indexOf(card.group_id));
+						removeGroupFromTable(getGroupById(card.group_id));
+						updateScore();
+					}
 				}
 			}
 		}
-		
+
 		// implementation for countdown timer
 		// tics every second
-		// when time_count reaches 0 ( initial time_count value = MAX_TIME ) computer gets the score
+		// when time_count reaches 0 ( initial time_count value = MAX_TIME )
+		// computer gets the score
 		class Task implements Runnable {
 
 			@Override
 			public void run() {
-				
-				// all UI changes need to be run on UI thread, 
+
+				// all UI changes need to be run on UI thread,
 				// so using a wrapper
 				runOnUiThread(new Runnable() {
-				     @Override
-				     public void run() {
-				    	time_count --;
-				    	countdown_view.setText("Time left to find a match: " + time_count);
-						
-						if(time_count == 0) {
-							computer_score ++;
-					    	endTurn();
+					@Override
+					public void run() {
+						time_count--;
+						countdown_view.setText("Time left to find a match: "
+								+ time_count);
+
+						if (time_count == 0) {
+							computer_score += matching_groups_ids.size();
+							endTurn();
 						}
-				    	 
-				     }
+
+					}
 				});
 			}
 		}
-		
-		private void endTurn(){
+
+		private void updateScore() {
+			score_view.setText(user_score + ":" + computer_score);
+		}
+
+		private void endTurn() {
+			Log.i("endTurn", "endTurn");
+
 			future_task.cancel(true);
-			
-			//clear countdown
+
+			// clear countdown
 			countdown_view.setText("");
-			
-			//update score
-			score_view.setText(user_score  + ":" + computer_score);
-			
-			//remove matching cards
-			matching_card.setVisibility(View.INVISIBLE);
-			current_card.setVisibility(View.INVISIBLE);
-			matching_card = null;
-			current_card = null;
-			
-			//check end game
-			int turns = user_score + computer_score;
-			if(turns == ROW_COUNT * COL_COUNT / 2){
-				String msg;
-				
-				if(user_score < computer_score) {
+
+			updateScore();
+
+			// remove matching cards
+			for (int id : matching_groups_ids) {
+				removeGroupFromTable(getGroupById(id));
+			}
+			removeGroupFromTable(current_group);
+			matching_groups_ids.clear();
+			current_group = null;
+
+			// check end game
+			checkEndGame(1000);
+		}
+
+		private void checkEndGame(int delay) {
+			int count_open = 0;
+			Card c;
+			for (int y = 0; y < ROW_COUNT; y++) {
+				for (int x = 0; x < COL_COUNT; x++) {
+					c = cards[y][x];
+
+					if (c.isUp) {
+						count_open++;
+					}
+				}
+			}
+			if (count_open == ROW_COUNT * COL_COUNT) {
+				final String msg;
+
+				if (user_score < computer_score) {
 					msg = "Defeat...";
-				}
-				else if(user_score > computer_score) {
+				} else if (user_score > computer_score) {
 					msg = "Victory!";
-				}
-				else {
+				} else {
 					msg = "Draw.";
 				}
-				
-				// show end game messages
-				end_game_message.setText(msg);
-				game_grid_container.setVisibility(View.GONE);
-				end_game_message_container.setVisibility(View.VISIBLE);
-				reset_btn_container.setVisibility(View.VISIBLE);
+
+				countdown_timer.schedule(new Runnable() {
+					@Override
+					public void run() {
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								// show end game messages
+								end_game_message.setText(msg);
+								game_grid_container.setVisibility(View.GONE);
+								end_game_message_container
+										.setVisibility(View.VISIBLE);
+								reset_btn_container.setVisibility(View.VISIBLE);
+							}
+						});
+					}
+				}, delay, TimeUnit.MILLISECONDS);
 			}
-			
-			
 		}
 	}
 }
